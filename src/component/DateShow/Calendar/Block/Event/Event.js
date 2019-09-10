d76485aee5d154355d69d2494fcd078d4f0cb2ee
Kafuu Chino <masterspark@openingsource.org>
2019-09-10 16:45:26 +0800

    add event`s change

--- src/component/DateShow/Calendar/Block/Event/Event.js
+++ src/component/DateShow/Calendar/Block/Event/Event.js
@@ -14,17 +14,53 @@ export default class Event extends React.Component{
             keynumber:this.props.keynumber,
             title:this.props.title,
             message:this.props.message,
-            datetime:datetime
+            datetime:datetime,
+            configing:false,
+            titleOnChange:'',
+            messageOnChange:''
         });
-        this.editcontent=this.editcontent.bind(this);
+        this.EditContent=this.EditContent.bind(this);
+        this.ConfigComplete=this.ConfigComplete.bind(this);
+        this.ReloadTitle=this.ReloadTitle.bind(this);
+        this.ReloadMessage=this.ReloadMessage.bind(this);
+        this.ConfigEvent=this.ConfigEvent.bind(this);
     }
 
-    editcontent(event){
+    EditContent(event){
         event.stopPropagation();
+        this.setState({
+            configing:true
+        })
+    }
+
+    ConfigComplete(){
+        this.setState({
+            configing:false
+        })
+    }
+
+    ReloadTitle(event){
+        this.setState({titleOnChange: event.target.value});
+    }
+    ReloadMessage(event){
+        this.setState({messageOnChange: event.target.value});
+    }
+
+    ConfigEvent(){
+        this.setState({
+            title:this.state.titleOnChange,
+            message:this.state.messageOnChange,
+            configing:false
+        })
+        this.setState({
+            titleOnChange:'',
+            messageOnChange:''
+        })
     }
 
     render(){
         return (
+            <>
             <Card>
                 <Accordion.Toggle as={Card.Header} eventKey={this.state.keynumber}>
                     <div className={styles.parent}>
@@ -32,7 +68,7 @@ export default class Event extends React.Component{
                             [{this.state.datetime.getFullYear()}-{this.state.datetime.getMonth()}-{this.state.datetime.getDate()}] [{this.state.datetime.getHours()}:{this.state.datetime.getMinutes()}] {this.state.title}
                         </div>
                         <div className={styles.edit}>
-                            <svg onClick={this.editcontent} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <svg onClick={this.EditContent} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <g>
                                 <path fill="none" d="M0 0h24v24H0z"/>
                                 <path d="M7.243 18H3v-4.243L14.435 2.322a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414L7.243 18zM3 20h18v2H3v-2z"/>
@@ -47,6 +83,30 @@ export default class Event extends React.Component{
                     </Card.Body>
                 </Accordion.Collapse>
             </Card>
+
+            <Modal show={this.state.configing} onHide={this.ConfigComplete}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Change Event</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                <Form>
+                    <Form.Group controlId="NewTitle">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control value={this.state.titleOnChange} onChange={this.ReloadTitle} type="input"/>
+                    </Form.Group>
+                    <Form.Group controlId="NewMessage">
+                        <Form.Label>Message</Form.Label>
+                        <Form.Control value={this.state.messageOnChange} onChange={this.ReloadMessage} as="textarea" rows="3" />
+                    </Form.Group>
+                </Form>
+                    <Button variant="primary" block onClick={this.ConfigEvent}>
+                        Complete
+                    </Button>
+                </Modal.Body>
+                <Modal.Footer>
+                </Modal.Footer>
+              </Modal>
+            </>
         );
     }
 }
